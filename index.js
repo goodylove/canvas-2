e2fa3e713724fd6426302d8a1d6db5e6747d28e2
goodness <goodyc474@gmail.com>
2022-12-02 23:36:04 +0100

    add   interactive to canvas element

--- index.js
+++ index.js
@@ -1,73 +1,81 @@
 // creating and resizing canvas
-const  canvas =  document.querySelector('canvas')
+const canvas = document.querySelector("canvas");
 
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
 
-canvas.width = window.innerWidth -100
-canvas.height = window.innerHeight -100
+const ctx = canvas.getContext("2d");
 
-let ctx = canvas.getContext('2d');
-
+const mouse = {};
+function handleMouseMove(e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+}
+addEventListener("mousemove", handleMouseMove);
 // drawing element
- 
-
-
 
 //  to draw multiple  cirlce  we will use    class constructor
 class Circle {
-constructor(x ,y, dx,dy, radius){
-this.x = x;
-this.y = y;
-this.dx = dx;
-this.dy = dy;
-this.radius = radius;
+  constructor(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.minRadius = radius;
+  }
 
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.stroke();
+    ctx.fill();
+  }
+  upDatePosition() {
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      this.radius = this.radius < 50 ? this.radius + 1 : this.radius;
+    } else {
+      this.radius =
+        this.radius > this.minRadius ? this.radius - 1 : this.radius;
     }
 
-    draw(){
-        ctx.beginPath()
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2, false )
-        ctx.stroke()
-        ctx.fillStyle = 'green'
-
-    }
-    upDatePosition(){
-
-        if(this.x + this.radius > canvas.width ||this.x - this.radius  < 0){
-            this.dx = -this.dx
-        }
-        if(this.y + this.radius > canvas.height || this.y - this.radius < 0){ 
-            this.dy = -this.dy
-        }
-        this.x += this.dx
-       this.y += this.dy
-       this.draw()
-        
-    }
+    this.draw();
+  }
 }
 
-let arrCircle = []
+let arrCircle = [];
 
-for (let i = 0; i < 100; i++) {
-    let radius  =  30;
-    let x = Math.random ()* ( canvas.width - radius * 2) + radius
-    let y = Math.random ()* (canvas.height - radius * 2)  + radius
-    let dx = (Math.random() - 0.5) 
-    let dy = (Math.random() - 0.5) 
-    arrCircle.push(new Circle(x,y ,dx,dy, radius))
+for (let i = 0; i < 700; i++) {
+  let radius = Math.random() * 3 + 2;
+  let x = Math.random() * (canvas.width - radius * 2) + radius;
+  let y = Math.random() * (canvas.height - radius * 2) + radius;
+  let dx = Math.random() - 0.5;
+  let dy = Math.random() - 0.5;
+  arrCircle.push(new Circle(x, y, dx, dy, radius));
 }
-
 
 //  let circle = new Circle(200, 100 ,30, 3,3 ,)
 
-function animate(){
-    ctx.clearRect(0,0,innerWidth,innerHeight)
-    requestAnimationFrame(animate)
-    for (let i = 0; i < arrCircle.length; i++) {
-        arrCircle[i].upDatePosition()
-        
-    }
-
-   
+function animate() {
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  requestAnimationFrame(animate);
+  for (let i = 0; i < arrCircle.length; i++) {
+    arrCircle[i].upDatePosition();
+  }
 }
-animate()
-
+animate();
