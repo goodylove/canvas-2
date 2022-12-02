640b5724a18384475ffbb20a3a7ae3e1d39cfb23
goodness <goodyc474@gmail.com>
2022-12-02 20:08:59 +0100

    make canvas take the full screen

--- index.js
+++ index.js
@@ -8,28 +8,66 @@ canvas.height = window.innerHeight -100
 let ctx = canvas.getContext('2d');
 
 // drawing element
- let x = 100;
- let y = 100;
- let dx = 4;
- let dy = 4;
- let radius  =  30;
+ 
 
+
+
+//  to draw multiple  cirlce  we will use    class constructor
+class Circle {
+constructor(x ,y, dx,dy, radius){
+this.x = x;
+this.y = y;
+this.dx = dx;
+this.dy = dy;
+this.radius = radius;
+
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2, false )
+        ctx.stroke()
+        ctx.fillStyle = 'green'
+
+    }
+    upDatePosition(){
+
+        if(this.x + this.radius > canvas.width ||this.x - this.radius  < 0){
+            this.dx = -this.dx
+        }
+        if(this.y + this.radius > canvas.height || this.y - this.radius < 0){ 
+            this.dy = -this.dy
+        }
+        this.x += this.dx
+       this.y += this.dy
+       this.draw()
+        
+    }
+}
+
+let arrCircle = []
+
+for (let i = 0; i < 100; i++) {
+    let radius  =  30;
+    let x = Math.random ()* ( canvas.width - radius * 2) + radius
+    let y = Math.random ()* (canvas.height - radius * 2)  + radius
+    let dx = (Math.random() - 0.5) 
+    let dy = (Math.random() - 0.5) 
+    arrCircle.push(new Circle(x,y ,dx,dy, radius))
+}
+
+
+//  let circle = new Circle(200, 100 ,30, 3,3 ,)
 
 function animate(){
-    requestAnimationFrame(animate)
     ctx.clearRect(0,0,innerWidth,innerHeight)
-    ctx.beginPath()
-ctx.arc(x,y,radius,0,Math.PI * 2, false )
-ctx.stroke()
+    requestAnimationFrame(animate)
+    for (let i = 0; i < arrCircle.length; i++) {
+        arrCircle[i].upDatePosition()
+        
+    }
 
-if( x + radius > canvas.width || x - radius  < 0){
-    dx = -dx
-}
-if(y + radius > canvas.height || y - radius < 0){ 
-    dy = -dy
-}
-x += dx
-y += dy
+   
 }
 animate()
 
